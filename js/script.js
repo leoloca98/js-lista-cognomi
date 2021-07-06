79970561a8dcbc6411f9a9c7c8bb3083deb374c0
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-06 17:42:40 +0200

    Complete(only js)

--- js/script.js
+++ js/script.js
@@ -8,3 +8,45 @@ Consegna:
 
 */
 
+// *Variabili
+var display = document.getElementById("listItems");
+var message = "Inserisci il tuo cognome: ";
+
+// *Creiamo un array contenente altri cognomi
+var vettLastName = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+// *Chiediamo il cognome all'utente
+var userLastName;
+
+// *Validazione (controllare se viene inserito un qualcosa, e non null/undefined...)
+// *Trasformo in minuscolo il cognome che inserirà
+do {
+    userLastName = prompt(message).toLowerCase().trim();
+} while (!userLastName);
+
+// *Faccio il capitalize tagliando e uppercasendo la prima lettera della parola
+var userLastNameCapitalized = userLastName.charAt().toUpperCase() + userLastName.slice(1);
+
+// * Pusho il cognome alla fine dell'array
+vettLastName.push(userLastNameCapitalized);
+
+// * Ordino alfabeticamente la lista di cognomi(compreso quello inserito dall'utente)
+vettLastName.sort();
+console.log("L'array ordinato alfabeticamente è così:");
+
+// *Stampo la posizione dell'ultimo elemento aggiunto (il cognome dell'utente)
+// *Stampo la lista ordinata alfabeticamente
+var i = 0;
+var position;
+
+for (i = 0; i < 6; i++) {
+    if (userLastNameCapitalized === vettLastName[i]) {
+        position = i + 1;
+    }
+    console.log(vettLastName[i]);
+}
+
+console.log("Il cognome dell'utente si trova in posizione :" + position);
+
+
+
